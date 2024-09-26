745d859c7579a1e169132cbe31a5b97128109893
pseudoyu <pseudoyu@connect.hku.hk>
2024-09-26 13:05:03 +0800

    feat: add ua and x-forwarded-for

--- main.go
+++ main.go
@@ -45,31 +45,37 @@ func handleProxyRequest(c echo.Context) error {
 
 	parsedURL, err := url.Parse(targetURL)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "Invalid URL")
+		return c.String(http.StatusBadRequest, fmt.Sprintf("Invalid URL: %v", err))
 	}
 
 	req, err := http.NewRequestWithContext(c.Request().Context(), "GET", targetURL, nil)
 	if err != nil {
-		return c.String(http.StatusInternalServerError, "Error creating request")
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Error creating request: %v", err))
 	}
 
 	setRefererHeader(req, parsedURL)
+	setUserAgentHeader(req)
+	setXForwardedForHeader(req, c.RealIP())
 
 	resp, err := getHTTPClient().Do(req)
 	if err != nil {
-		return c.String(http.StatusBadGateway, "Error fetching the URL")
+		return c.String(http.StatusBadGateway, fmt.Sprintf("Error fetching the URL: %v", err))
 	}
 	defer resp.Body.Close()
 
-	if !strings.Contains(resp.Header.Get("Content-Type"), "image") {
-		return c.String(http.StatusForbidden, "Forbidden")
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "image") {
+		return c.String(http.StatusForbidden, fmt.Sprintf("Forbidden: Content-Type is not an image. Received Content-Type: %s", contentType))
 	}
 
 	c.Response().Header().Set("Content-Type", resp.Header.Get("Content-Type"))
 	c.Response().WriteHeader(resp.StatusCode)
 
 	_, err = io.Copy(c.Response().Writer, resp.Body)
-	return err
+	if err != nil {
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Error copying response body: %v", err))
+	}
+	return nil
 }
 
 func setRefererHeader(req *http.Request, parsedURL *url.URL) {
@@ -78,4 +84,12 @@ func setRefererHeader(req *http.Request, parsedURL *url.URL) {
 		mainDomain := strings.Join(hostnameParts[len(hostnameParts)-2:], ".")
 		req.Header.Set("Referer", fmt.Sprintf("https://%s", mainDomain))
 	}
+}
+
+func setUserAgentHeader(req *http.Request) {
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
+}
+
+func setXForwardedForHeader(req *http.Request, realIP string) {
+	req.Header.Set("X-Forwarded-For", realIP)
 }
